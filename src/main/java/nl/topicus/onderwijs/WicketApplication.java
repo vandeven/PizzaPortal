a098a9a89fa4bb6e2af9808deb15bf1aa3d437f7
Joost L <joost.limburg@topicus.nl>
2012-12-07 14:12:48 +0100

    Persistance globally running

--- src/main/java/nl/topicus/onderwijs/WicketApplication.java
+++ src/main/java/nl/topicus/onderwijs/WicketApplication.java
@@ -1,20 +1,28 @@
 package nl.topicus.onderwijs;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this application
+ * without deploying, run the Start class.
  * 
  * @see nl.topicus.onderwijs.Start#main(String[])
  */
 public class WicketApplication extends WebApplication
-{    	
+{
+	private static final String PERSISTENCE_UNIT_NAME = "todos";
+
+	private static EntityManagerFactory factory;
+
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
 	@Override
-	public Class<? extends WebPage> getHomePage()
+	public Class< ? extends WebPage> getHomePage()
 	{
 		return HomePage.class;
 	}
@@ -28,5 +36,16 @@ public class WicketApplication extends WebApplication
 		super.init();
 
 		// add your configuration here
+		setPersistenceFactory(Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME));
+	}
+
+	public static EntityManagerFactory getPersistenceFactory()
+	{
+		return factory;
+	}
+
+	public static void setPersistenceFactory(EntityManagerFactory factory)
+	{
+		WicketApplication.factory = factory;
 	}
 }
